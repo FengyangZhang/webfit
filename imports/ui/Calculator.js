ff01bd9de746fdf1b67b7ac89a82ecae71caec31
FengyangZhang <fz2ds@virginia.edu>
2018-10-15 19:44:26 -0400

    basic form utility

--- imports/ui/Calculator.js
+++ imports/ui/Calculator.js
@@ -3,38 +3,45 @@ export default class Calculator extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            bmi: ""
+            calculated: false,
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
+        this.setState({
+            calculated: true,
+        })
     }
-    
+  
     render(){
         return(
-            <div>         
-                <form className = "calForm">
-                    <div>
-                        <p>
-                            <label>Weight(kg): </label>
-                            <input type = "text" name = "weight" placeholder = "" />
-                        </p>  
-                        <p>
-                            <label>Height(cm): </label>
-                            <input type = "text" name = "height" placeholder = "" />
-                        </p>  
-                        <p>
-                            <label>Age(years): </label>
-                            <input type = "text" name = "age" placeholder = "" />
-                        </p>
-                        <input type="submit" value="Submit" />  
-                    </div>
-                </form>
-            </div>
+            this.state.calculated ?
+            (
+                <div/>
+            ) : (
+                <div>         
+                    <form className = "calForm">
+                        <div>
+                            <p>
+                                <label>Weight(kg): </label>
+                                <input type = "text" name = "weight"  placeholder = "" />
+                            </p>  
+                            <p>
+                                <label>Height(cm): </label>
+                                <input type = "text" name = "height" placeholder = "" />
+                            </p>  
+                            <p>
+                                <label>Age(years): </label>
+                                <input type = "text" name = "age" placeholder = "" />
+                            </p>
+                            <input type="submit" value="Submit" onClick={this.handleSubmit}/>  
+                            <input type="reset" value="Clear" />
+                        </div>
+                    </form>
+                </div>
+            )
         );
     }
 }
