861a7c69d8c63cc378bee73758ea695653d53ccd
FengyangZhang <fz2ds@virginia.edu>
2018-10-16 16:47:40 -0400

    dispplay result

--- imports/ui/Calculator.js
+++ imports/ui/Calculator.js
@@ -3,40 +3,100 @@ export default class Calculator extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
+            weight: "",
+            height: "",
+            age: "",
             calculated: false,
+            bmi: "",
+            gender: "male",
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleHeightChange = this.handleHeightChange.bind(this);
+        this.handleWeightChange = this.handleWeightChange.bind(this);
+        this.handleMaleChange = this.handleMaleChange.bind(this);
+        this.handleFemaleChange = this.handleFemaleChange.bind(this);
+        this.handleAgeChange = this.handleAgeChange.bind(this);
     }
 
     handleSubmit(event) {
+        height = this.state.height;
+        weight = this.state.weight;
+        age = this.state.age;
+        bmi = weight / (height/100 * height/100);
+        gender = this.state.gender;
+        let bmr;
+        if(gender === "male") {
+            bmr = 13.7 * weight + 5.0 * height - 6.8 * age + 66;
+        }
+        else {
+            bmr = 9.6 * weight + 1.8 * height - 4.7 * age + 655;
+        }
         this.setState({
             calculated: true,
+            bmi: bmi,
+            bmr: bmr,
         })
     }
-  
+
+    handleHeightChange(event) {
+        this.setState({height: event.target.value});
+    }
+    handleWeightChange(event) {
+        this.setState({weight: event.target.value});
+    }
+    handleAgeChange(event) {
+        this.setState({age: event.target.value});
+    }
+    handleMaleChange(event) {
+        this.setState({gender: "male"});
+    }
+    handleFemaleChange(event) {
+        console.log(event.target.value);
+        this.setState({gender: "female"});
+    }
     render(){
         return(
             this.state.calculated ?
             (
-                <div/>
+                <div>
+                    <b>Your BMI is {this.state.bmi}, calculated as {this.state.weight}kg / ({this.state.height/100}m ^ 2).</b>
+                    <br/>
+                    <b>Your BMR is {this.state.bmr}, calculated as 
+                        {this.state.gender === "male" ? (
+                            13.7 + "*" + this.state.weight + "kg" + "+" + 5.0 + "*" + this.state.height + "cm" 
+                                + "-" + 6.8 + "*" + this.state.age + "years" + "+" + 66 + "."
+                        ) :(
+                            9.6 + "*" + this.state.weight + "kg" + "+" + 1.8 + "*" + this.state.height + "cm" 
+                                + "-" + 4.7 + "*" + this.state.age + "years" + "+" + 655 + "."
+                        )}
+                    </b>
+                </div>
             ) : (
                 <div>         
                     <form className = "calForm">
                         <div>
+                            <b><text>Input your information to get you BMI and BMR: </text></b>
                             <p>
                                 <label>Weight(kg): </label>
-                                <input type = "text" name = "weight"  placeholder = "" />
+                                <input type = "text" name = "weight"  placeholder = "" onChange ={this.handleWeightChange}/>
                             </p>  
                             <p>
                                 <label>Height(cm): </label>
-                                <input type = "text" name = "height" placeholder = "" />
+                                <input type = "text" name = "height" placeholder = "" onChange ={this.handleHeightChange}/>
                             </p>  
                             <p>
                                 <label>Age(years): </label>
-                                <input type = "text" name = "age" placeholder = "" />
+                                <input type = "text" name = "age" placeholder = "" onChange ={this.handleAgeChange}/>
                             </p>
-                            <input type="submit" value="Submit" onClick={this.handleSubmit}/>  
+                            <p>
+                                <label>Gender: </label>
+                                <input type = "radio" name = "gender" onChange={this.handleMaleChanged}
+                                    defaultChecked={this.state.gender === "male"} /> male     
+                                <input type = "radio" name = "gender" onChange={this.handleFemaleChanged}
+                                    defaultChecked={this.state.gender != "male"} /> female
+                            </p>
+                            <input type="button" value="Submit" onClick={this.handleSubmit}/>  
                             <input type="reset" value="Clear" />
                         </div>
                     </form>
