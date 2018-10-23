e7ef5195500b2be7ffe8156448cf28e38d283374
yutong wang <31590637+elvawyt@users.noreply.github.com>
2018-10-23 17:57:24 -0400

    login and sign up page

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import React, { Component } from "react";
+import {Link} from 'react-router';
+import TitleBar from './../ui/TitleBar';
+import {Accounts} from 'meteor/accounts-base';
 export default class Login extends React.Component{
-    render(){
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          uid: "",
+          password: ""
+        };
+      }
+    
+      validateForm() {
+        return this.state.uid.length > 0 && this.state.password.length > 0;
+      }
+    
+      handleChange = event => {
+        this.setState({
+          [event.target.id]: event.target.value
+        });
+      }
+    
+      handleSubmit = event => {
+        event.preventDefault();
+      }
+        render(){
     return (
         <div>
-        <p> this is login page </p>
-        </div>
+
+        <TitleBar title = "Lemon fitness"/>
+        <div className="Login">      
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="uid" bsSize="large">
+            <ControlLabel>Username</ControlLabel>
+            <FormControl
+              autoFocus
+              type="uid"
+              value={this.state.uid}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+          <Link to = "Signup">Don't have an account?</Link>
+        </form>
+      </div>
+    </div>
     );
     }
 }
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Link} from 'react-router';
+import TitleBar from './../ui/TitleBar';
+import {Accounts} from 'meteor/accounts-base';
+
+
 export default class Signup extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            error : ''
+        };
+    }
+    onSubmit(e){
+        e.preventDefault();
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+        Accounts.createUser({email,password},(err)=>{
+            console.log('Signup callback',err);
+        });
+        
+    }
+
     render(){
         return (
-        <div>
-        <p> this is sign up page </p>
-        </div>
+            <div>
+            <TitleBar title = "Lemon fitness"/>
+            <form onSubmit = {this.onSubmit.bind(this)}>
+                <input type = "email" ref = "email" name = "email" placeholder = "Email"/>
+                <input type = "password" ref = "password" name = "password" placeholder = "Password"/>
+                <button> Creat Account </button>
+            </form>
+            <Link to = "/"> Already have an account? </Link>
+            </div>
+       
     );
 
     }
